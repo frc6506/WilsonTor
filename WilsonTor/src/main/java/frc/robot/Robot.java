09b3dfd166863638c3fabb1033fb3dd8d9221a0e
LePlebClam <59941350+LePlebClam@users.noreply.github.com>
2020-02-10 15:56:53 -0800

    changed rotate to angle to 90

--- WilsonTor/src/main/java/frc/robot/Robot.java
+++ WilsonTor/src/main/java/frc/robot/Robot.java
@@ -85,7 +85,6 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void autonomousInit() {}
-
   /** This function is called periodically during autonomous. */
   @Override
   public void autonomousPeriodic() {
--- WilsonTor/src/main/java/frc/robot/commands/RotateToAngle.java
+++ WilsonTor/src/main/java/frc/robot/commands/RotateToAngle.java
@@ -24,7 +24,7 @@ public class RotateToAngle extends Command {
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
-    Robot.drivetrain.trackTarget();
+    Robot.drivetrain.rotateToAngle(90);
   }
 
   // Make this return true when this Command no longer needs to run execute()
